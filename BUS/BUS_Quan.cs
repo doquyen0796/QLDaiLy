9b21017e681b37efa2707b61d7f04d024930fed4
Huynh Trung Nghia <huynhtrungnghia961996@gmail.com>
2018-03-29 23:19:16 +0700

    [Cài đặt] Lưu số đại lý tối đa

--- BUS/BUS_Quan.cs
+++ BUS/BUS_Quan.cs
@@ -21,5 +21,34 @@ namespace BUS
 
             return quan;
         }
+
+
+        /// <summary>
+        /// Lấy số đại lý tối đa trong quận (HIỆN TẠI)
+        /// </summary>
+        /// <returns></returns>
+        public int LaySoDaiLyToiDa()
+        {
+            var q = db.Quans.FirstOrDefault();
+
+            return (int)q.SoDaiLyToiDa;
+        }
+
+
+        /// <summary>
+        /// Lưu số đại lý tối đa trong quận
+        /// </summary>
+        /// <param name="newmax"></param>
+        /// <returns></returns>
+        public bool LuuSoDaiLyToiDa(int newmax)
+        {
+            //  https://stackoverflow.com/questions/10314552/how-to-update-the-multiple-rows-at-a-time-using-linq-to-sql
+            var q = db.Quans.ToList();
+
+            q.ForEach(a => a.SoDaiLyToiDa = newmax);
+            db.SaveChanges();
+
+            return true;
+        }
     }
 }
--- QLDaiLy/frmSoDaiLyToiDa.cs
+++ QLDaiLy/frmSoDaiLyToiDa.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using BUS;
 
 namespace QLDaiLy
 {
@@ -21,19 +22,43 @@ namespace QLDaiLy
 
         private void txtSoDLToiDa_KeyPress(object sender, KeyPressEventArgs e)
         {
-
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false)
+            {
+                e.Handled = true;
+            }
         }
 
 
         private void btnLuu_Click(object sender, EventArgs e)
         {
-
+            BUS_Quan q = new BUS_Quan();
+            int oldmax = q.LaySoDaiLyToiDa();
+            int newmax = int.Parse(txtSoDLToiDa.Text);
+            if (oldmax != newmax)
+            {
+                var tb = MessageBox.Show(string.Format("Hiện tại, số đại lý tối đa trong một quận là {0}.\nBạn có chắc chắn muốn chỉnh sửa thành {1} ?", oldmax.ToString(), newmax.ToString()), "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (tb == DialogResult.Yes)
+                {
+                    q.LuuSoDaiLyToiDa(newmax);
+                    MessageBox.Show("Bạn đã lưu thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                this.Close();
+            }
         }
 
 
         private void frmSoDaiLyToiDa_Load(object sender, EventArgs e)
         {
-
+            BUS_Quan q = new BUS_Quan();
+            txtSoDLToiDa.Text = q.LaySoDaiLyToiDa().ToString();
         }
     }
 }
